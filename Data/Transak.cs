1d75d921b959504d0780863d25403fb077c467db
zjelev <zlatomir@gmail.com>
2023-10-19 08:24:42 +0300

    remove redundant columns

--- Data/Transak.cs
+++ Data/Transak.cs
@@ -21,35 +21,36 @@ public class Transak
 
     public DateTime? DateTrans { get; set; }
 
-    public string? TransNumber { get; set; }
+    // public string? TransNumber { get; set; } // Номер на пътен лист - има го в PList
 
     public double? KmKm { get; set; }
 
-    public double? OsnovnaTrans { get; set; }
+    // Всички тези са излишни, те са KmKм за съответното KmId
+    //public double? OsnovnaTrans { get; set; }
 
-    public double? RudnikTrans { get; set; }
+    //public double? RudnikTrans { get; set; }
 
-    public double? OkragTrans { get; set; }
+    //public double? OkragTrans { get; set; }
 
-    public double? StolicaTrans { get; set; }
+    //public double? StolicaTrans { get; set; }
 
-    public double? GradskaTrans { get; set; }
+    //public double? GradskaTrans { get; set; }
 
-    public double? MqstoTrans { get; set; }
+    //public double? MqstoTrans { get; set; }
 
-    public double? KlimaTrans { get; set; }
+    //public double? KlimaTrans { get; set; }
 
-    public double? AgregatTrans { get; set; }
+    //public double? AgregatTrans { get; set; }
 
-    public double? Zarabotka { get; set; }
+    //public double? Zarabotka { get; set; }
 
-    public double? Izvan { get; set; }
+    //public double? Izvan { get; set; }
 
-    public double? Doma { get; set; }
+    //public double? Doma { get; set; }
 
-    public double? KlimatikTrans { get; set; }
+    //public double? KlimatikTrans { get; set; }
 
-    public double? PechkaTrans { get; set; }
+    //public double? PechkaTrans { get; set; }
 
     public string? User { get; set; }
 
--- Models/TransakModel.cs
+++ Models/TransakModel.cs
@@ -6,14 +6,8 @@ public class TransakModel
 {
     public int Id { get; set; }
 
-    public int MotoId { get; set; }
-    public Moto Moto { get; set; }
-
     public int OtdelId { get; set; }
     public Otdel Otdel { get; set; }
-
-    public int SlujitelId { get; set; }
-    public Slujitel Slujitel { get; set; }
 
     public int KmId { get; set; }
     public Kilometri Km { get; set; }
@@ -26,32 +20,6 @@ public class TransakModel
     public string? TransNumber { get; set; }
 
     public double? KmKm { get; set; }
-
-    public double? OsnovnaTrans { get; set; }
-
-    public double? RudnikTrans { get; set; }
-
-    public double? OkragTrans { get; set; }
-
-    public double? StolicaTrans { get; set; }
-
-    public double? GradskaTrans { get; set; }
-
-    public double? MqstoTrans { get; set; }
-
-    public double? KlimaTrans { get; set; }
-
-    public double? AgregatTrans { get; set; }
-
-    public double? Zarabotka { get; set; }
-
-    public double? Izvan { get; set; }
-
-    public double? Doma { get; set; }
-
-    public double? KlimatikTrans { get; set; }
-
-    public double? PechkaTrans { get; set; }
 
     public string? User { get; set; }
 
