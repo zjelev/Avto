8ce347c8e76a52be5ff26965ea7f7c47ba54314a
zjelev <zlatomir@gmail.com>
2023-11-01 16:00:02 +0200

    Add Search ViewComponent

---
+++ ViewComponents/Search.cs
@@ -0,0 +1,13 @@
+﻿using Avto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avto.ViewComponents;
+
+public class Search : ViewComponent
+{
+    public IViewComponentResult Invoke(PListModel searchModel)
+    {
+        ViewData["SearchModel"] = searchModel;
+        return View(searchModel);
+    }
+}
