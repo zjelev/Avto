bdb400cece81aa95fffc3192fd7345db69583bf9
zjelev <zlatomir@gmail.com>
2023-11-09 14:09:07 +0200

    add more fields to search in Transaks

--- Controllers/TransaksController.cs
+++ Controllers/TransaksController.cs
@@ -25,8 +25,26 @@ public class TransaksController : BaseController<TransakModel, Transak>
 
     protected override IQueryable<Transak> ApplyCustomSearch(SearchModel searchModel)
     {
-        var query = _context.Transaks.
-            Where(l => l.PList.Data > DateTime.Today.AddYears(-1));
+        var query = _context.Transaks
+            .Where(l => l.PList.Data > DateTime.Today.AddYears(-1));
+
+        if (!string.IsNullOrEmpty(searchModel.Number))
+            query = query.Where(l => l.PList.Number.Contains(searchModel.Number));
+
+        if (searchModel.From.HasValue)
+            query = query.Where(l => l.PList.Data >= ToNullableDateTime(searchModel.From.Value));
+
+        if (searchModel.To.HasValue)
+            query = query.Where(l => l.PList.Data <= ToNullableDateTime(searchModel.To.Value));
+
+        if (!string.IsNullOrEmpty(searchModel.MotoName))
+            query = query.Where(l => l.PList.Moto.Name.Contains(searchModel.MotoName));
+
+        if (!string.IsNullOrEmpty(searchModel.MotoNumber))
+            query = query.Where(l => l.PList.Moto.Number.Contains(searchModel.MotoNumber));
+
+        if (!string.IsNullOrEmpty(searchModel.SlujitelName))
+            query = query.Where(l => l.PList.Slujitel.Name.Contains(searchModel.SlujitelName));
 
         if (!string.IsNullOrEmpty(searchModel.Otdel))
             query = query.Where(t => t.Otdel.Name.Contains(searchModel.Otdel));
